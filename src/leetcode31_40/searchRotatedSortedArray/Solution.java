b7c6c341b6760229b4ff1e3db005924c15d64c92
RoronoalZoro <90810352+RoronoalZoro@users.noreply.github.com>
2024-03-05 11:28:40 +0700

    commit

--- src/leetcode31_40/searchRotatedSortedArray/Solution.java
+++ src/leetcode31_40/searchRotatedSortedArray/Solution.java
@@ -2,24 +2,10 @@ package leetcode31_40.searchRotatedSortedArray;
 
 public class Solution {
     public static int search(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (target == nums[mid]) {
-                return mid;
-            }
-            if (nums[mid] >= nums[left]) {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if (target > nums[mid] && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (target == nums[i]) {
+                return i;
             }
         }
         return -1;
